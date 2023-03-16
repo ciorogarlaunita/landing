bf9a568dfe06e358b2e19a828ca3e953ec6830c2
Savin Angel-Mario <savin@angelmario.eu>
2023-03-16 14:13:51 +0000

    Added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 import { Poppins } from "next/font/google"
 
 const poppins = Poppins({
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
 			</Head>
 			<main className={`${poppins.variable} font-sans`}>
 				<Component {...pageProps} />
+				<Analytics />
 			</main>
 		</>
 	)
