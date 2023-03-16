641349b5e8b6c9b794d98396878d4d7fa6aeb684
Savin <savin@angelmario.eu>
2023-03-16 15:10:13 +0200

    Fixed property in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
 	dest: "public",
-	serviceWorker: "service-worker.js",
+	sw: "service-worker.js",
 	disable: process.env.NODE_ENV === "development",
 	runtimeCaching: [
 		{
